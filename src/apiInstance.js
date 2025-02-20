13c6b4e06fbea9a0472da44a99be42d3b40c73eb
Hoang Thanh Tung <tunghoang25073012@gmail.com>
2025-02-20 22:34:19 +0700

    change to local

--- src/apiInstance.js
+++ src/apiInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axioInstance = axios.create({
-  baseURL: 'https://clbbcq8.com/api/v1',
+  baseURL: 'http://localhost:8080/api/v1',
   withCredentials: true,
 });
 
