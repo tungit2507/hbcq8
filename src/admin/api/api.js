f192e2178d4d6ba263097dd607d70d2b46abe785
Hoang Thanh Tung <131683573+tungit2507@users.noreply.github.com>
2025-05-24 10:39:34 +0700

    change ip

--- src/admin/api/api.js
+++ src/admin/api/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://clbbcduaq8.com',
   withCredentials: true,
 });
 
 axiosInstance.interceptors.response.use(
   response => response,
   error => {
-    if(error.response.data.status == 401){
+    if (error.response.data.status == 401) {
       localStorage.removeItem('currentUser');
       localStorage.removeItem('isLoggedIn');
       window.location.href = '/login';
